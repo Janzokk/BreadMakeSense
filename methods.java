12b2445af79faf74e245bd425a282f995802b203
jpg <92806924+Janzokk@users.noreply.github.com>
2022-05-19 20:10:32 +0200

    Add files via upload

--- methods.java
+++ methods.java
@@ -1,6 +1,16 @@
+package p1;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 
 /**
@@ -8,23 +18,119 @@ import java.sql.Statement;
  */
 public class methods {
 
-    public static void main(String[] args) {
-        initializeBD();
-    }
+	static Connection con;
+	static Statement stmt;
+	static FileWriter fw;
+	static FileReader fr;
+	static BufferedReader br;
+	static HashMap<String, String> hm;
 
-    public static void initializeBD(){
-        try {   
-            Connection con = DriverManager.getConnection("jdbc:mysql://192.168.2.11:3306/BMS", "jan", "1234");
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select breads from server");
-        } catch (Exception e) {
-            System.err.println("Error: Can't connect to database");
-            e.printStackTrace();
-        }
-    }
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 
-    public void attempLogin(String u, String p) {
-    
-    }
-    
+		initializeBD();
+
+		long globalBreads = serverPuntuation();
+
+		try {
+			fw = new FileWriter("/media/jpg/5BCC-1467/sintesis/log.txt");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.print("Username: ");
+		String uname = in.nextLine();
+		System.out.print("Password: ");
+		String password = in.nextLine();
+
+		attempLogin(uname, password);
+	}
+
+	public static void initializeBD() {
+		try {
+			fr = new FileReader("/media/jpg/5BCC-1467/sintesis/config.conf");
+			br = new BufferedReader(fr);
+
+			String res = "";
+			String[] config;
+
+			hm = new HashMap<String, String>();
+
+			while ((res = br.readLine()) != null) {
+
+				config = res.split("=");
+				hm.put(config[0], config[1]);
+			}
+
+			con = DriverManager.getConnection(
+					"jdbc:mysql://" + hm.get("ip") + ":" + hm.get("port") + "/" + hm.get("bd"), hm.get("user"),
+					hm.get("passwd"));
+			stmt = con.createStatement();
+		} catch (Exception e) {
+			try {
+				fw.write("Error: Can't connect to database");
+			} catch (IOException ioe) {
+				ioe.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+	}
+
+	public static void attempLogin(String u, String p) {
+
+		boolean usFound = false;
+
+		try {
+			ResultSet getUname = stmt.executeQuery("select username from users");
+
+			while (getUname.next()) {
+
+				if (u.equals(getUname.getString(1))) {
+					usFound = true;
+					PreparedStatement pstmt = con.prepareStatement("select passwd from users where username = ?");
+					pstmt.setString(1, u);
+
+					ResultSet getPass = pstmt.executeQuery();
+					if (getPass.next()) {
+						if (p.equals(getPass.getString(1))) {
+							fw.write("Connection established");
+							System.out.println("Conected");
+						} else {
+							fw.write("Error: Incorrect password");
+						}
+					}
+				}
+			}
+			if (!usFound) {
+				PreparedStatement pstmt = con.prepareStatement("insert into users(username, passwd) values(?, ?)");
+				pstmt.setString(1, u);
+				pstmt.setString(2, p);
+
+				int newUser = pstmt.executeUpdate();
+
+			}
+
+		} catch (Exception e) {
+			try {
+				fw.write("Error: Can't connect to database");
+			} catch (IOException ioe) {
+				ioe.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+	}
+
+	public static long serverPuntuation() {
+		try {
+			ResultSet totalPunt = stmt.executeQuery("select breads from server limit 1");
+			if (totalPunt.next()) {
+				return totalPunt.getLong(1);
+			}
+		} catch (SQLException e) {
+			System.err.println("Can't get total breads");
+			e.printStackTrace();
+		}
+		return 0;
+
+	}
+
 }
