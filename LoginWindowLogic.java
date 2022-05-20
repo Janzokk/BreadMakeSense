5ea0a23ff014a9a25f7219069810c36fde2a583c
jpg <92806924+Janzokk@users.noreply.github.com>
2022-05-20 19:58:41 +0200

    javafx Login and methods finished

---
+++ LoginWindowLogic.java
@@ -0,0 +1,151 @@
+package bread;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+/**
+ * methods
+ */
+public class LoginWindowLogic {
+
+	static Connection con;
+	static Statement stmt;
+	static FileHandler handler;
+	static Logger logger;
+	static FileReader fr;
+	static BufferedReader br;
+	static HashMap<String, String> hm;
+	
+	static String clientVersion = "1.0";
+	long globalBreads = serverPuntuation();
+
+	public static void main(String[] args) {
+
+	}
+	public static void startLogic() {
+		startLog();
+		getConfig();
+		initializeBD();
+		
+	}
+	
+	public static void getConfig() {
+		try {
+			fr = new FileReader("/media/jpg/5BCC-1467/sintesis/config.conf");
+			br = new BufferedReader(fr);
+	
+			String res = "";
+			String[] config;
+	
+			hm = new HashMap<String, String>();
+	
+			while ((res = br.readLine()) != null) {
+	
+				config = res.split("=");
+				hm.put(config[0], config[1]);
+			}
+		}catch(IOException ioe) {
+			logger.warning("Can't acces to configuration file");
+		}
+	}
+
+	public static void initializeBD() {
+		try {
+
+			con = DriverManager.getConnection(
+					"jdbc:mysql://" + hm.get("ip") + ":" + hm.get("port") + "/" + hm.get("bd"), hm.get("user"),
+					hm.get("passwd"));
+			stmt = con.createStatement();
+		} catch (Exception e) {
+			logger.severe("Error: Can't connect to database");
+			e.printStackTrace();
+		}
+	}
+
+	public static void checkVersions() {
+		try {
+			ResultSet getVersion = stmt.executeQuery("select vers from server");
+			
+			if(!clientVersion.equals(getVersion.getString(1))) {
+				
+			}
+		} catch (SQLException e) {
+			logger.warning("Cant get version from the server");
+			e.printStackTrace();
+		}
+	}
+	
+	public static void startLog() {
+		try {
+			handler = new FileHandler("/media/jpg/5BCC-1467/sintesis/default.log");
+			logger = Logger.getLogger("p1");
+			logger.addHandler(handler);
+		} catch (SecurityException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void attempLogin(String u, String p) {
+
+		boolean usFound = false;
+
+		try {
+			ResultSet getUname = stmt.executeQuery("select username from users");
+
+			while (getUname.next()) {
+
+				if (u.equals(getUname.getString(1))) {
+					usFound = true;
+					PreparedStatement pstmt = con.prepareStatement("select passwd from users where username = ?");
+					pstmt.setString(1, u);
+
+					ResultSet getPass = pstmt.executeQuery();
+					if (getPass.next()) {
+						if (p.equals(getPass.getString(1))) {
+							logger.info("Connection established");
+						} else {
+							logger.warning("Error: Incorrect password");
+						}
+					}
+				}
+			}
+			if (!usFound) {
+				PreparedStatement pstmt = con.prepareStatement("insert into users(username, passwd) values(?, ?)");
+				pstmt.setString(1, u);
+				pstmt.setString(2, p);
+
+				int newUser = pstmt.executeUpdate();
+
+			}
+
+		} catch (Exception e) {
+			logger.severe("Error: Can't connect to database");
+		}
+	}
+
+	public static long serverPuntuation() {
+		try {
+			ResultSet totalPunt = stmt.executeQuery("select breads from server limit 1");
+			if (totalPunt.next()) {
+				return totalPunt.getLong(1);
+			}
+		} catch (SQLException e) {
+			logger.info("Can't get total breads");
+			e.printStackTrace();
+		}
+		return 0;
+
+	}
+
+}
