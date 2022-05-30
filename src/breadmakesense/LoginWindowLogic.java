a7d50c73bf53899b79e32a61475dbd042fd8342d
ferrerjoel <95489740+ferrerjoel@users.noreply.github.com>
2022-05-30 14:18:31 +0200

    Update LoginWindowLogic.java

--- src/breadmakesense/LoginWindowLogic.java
+++ src/breadmakesense/LoginWindowLogic.java
@@ -14,10 +14,11 @@ import java.sql.PreparedStatement;
 import java.sql.Statement;
 
 /**
- * methods
+ * @author jpg, ferrerjoel
+ * This class is the one with the logic used to log in.
  */
 public class LoginWindowLogic {
-
+	//Global variables declaration
 	static Connection con;
 	static Statement stmt;
 	static ResultSet getUname;
@@ -31,14 +32,18 @@ public class LoginWindowLogic {
 	static boolean incPass;
 	static String username;
 	static String loginInfo;
-
+	/**
+	 * Starts all the logic so we can use it anywhere.
+	 */
 	public static void startLogic() {
 		startLog();
 		getConfig();
 		initializeBD();
 		checkVersions();
 	}
-
+	/**
+	 *	Read the configuration file so we can use it later
+	 */
 	public static void getConfig() {
 		try {
 			fr = new FileReader(System.getProperty("user.dir") + "/src/files/config.conf");
@@ -48,7 +53,7 @@ public class LoginWindowLogic {
 			String[] config;
 
 			hm = new HashMap<String, String>();
-
+			//Separates the lines for the symbol. Links the rigth side with the left side
 			while ((res = br.readLine()) != null) {
 
 				config = res.split("=");
@@ -58,20 +63,25 @@ public class LoginWindowLogic {
 			logger.warning("Can't acces to configuration file");
 		}
 	}
-
+	/**
+	 * Initialize the DataBase
+	 */
 	public static void initializeBD() {
 		try {
-
+			//Uses the configuration file to connect to the database
 			con = DriverManager.getConnection(
 					"jdbc:mysql://" + hm.get("ip") + ":" + hm.get("port") + "/" + hm.get("bd"), hm.get("user"),
 					hm.get("passwd"));
+			//Also creates a statement for later
 			stmt = con.createStatement();
 		} catch (Exception e) {
 			logger.severe("Error: Can't connect to database");
 			e.printStackTrace();
 		}
 	}
-
+	/**
+	 * Check if the client version is up-to-date
+	 */
 	public static void checkVersions() {
 		try {
 
@@ -87,7 +97,9 @@ public class LoginWindowLogic {
 			e.printStackTrace();
 		}
 	}
-
+	/**
+	 * Start the Log file (all the info will go there)
+	 */
 	public static void startLog() {
 		try {
 			handler = new FileHandler(System.getProperty("user.dir") + "/src/logs/default.log");
@@ -97,15 +109,21 @@ public class LoginWindowLogic {
 			e.printStackTrace();
 		}
 	}
-
+	/**
+	 * Tries to see if the username and password are in the database and if there is no user, creates one.
+	 * Used to Log in
+	 * @param u The username
+	 * @param p The password
+	 */
 	public static void attempLogin(String u, String p) {
 
 		username = u;
 		boolean usFound = false, end = false;
-
+		//This while is in case the user have to be created it log in automatically
 		while (!end) {
 			try {
 				getUname = stmt.executeQuery("select username, id from users");
+				//Search for the username
 				while (getUname.next()) {
 					if (u.equals(getUname.getString(1))) {
 						usFound = true;
@@ -113,6 +131,7 @@ public class LoginWindowLogic {
 						pstmt.setString(1, u);
 
 						ResultSet getPass = pstmt.executeQuery();
+						//Checks if the password coincide
 						if (getPass.next()) {
 							if (p.equals(getPass.getString(1))) {
 								logger.info("Connection established");
@@ -125,7 +144,7 @@ public class LoginWindowLogic {
 						}
 					}
 				}
-
+				//If the user is not found it creates it
 				if (!usFound) {
 					PreparedStatement pstmt = con.prepareStatement("insert into users(username, passwd) values(?, ?)");
 					pstmt.setString(1, u);
@@ -144,7 +163,10 @@ public class LoginWindowLogic {
 		System.out.println("A");
 		loginInfo = (!usFound) ? "User created successfully" : "User logged successfully";
 	}
-
+	/**
+	 * Gets the sum of all the breads generated
+	 * @return
+	 */
 	public static double serverPuntuation() {
 		try {
 			ResultSet totalPunt = stmt.executeQuery("select sum(legacy_bread) from users");
@@ -158,7 +180,12 @@ public class LoginWindowLogic {
 		return 0;
 
 	}
-
+	/**
+	 * Checks if the username or the password have the requeriments
+	 * @param u The username
+	 * @param p The password
+	 * @return
+	 */
 	public static boolean loginInCheck(String u, String p) {
 		if (u.trim().equals("") || p.trim().equals("") || u.contains(" ") || p.contains(" ") || p.length() >= 16 || p.length() < 4|| u.length() < 4) {
 			return true;
